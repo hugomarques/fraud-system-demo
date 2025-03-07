d74478fb1aff8e36f1f14a786789fcaf0bfd5b3b
Hugo Marques <hmarques@netflix.com>
2025-03-07 18:08:16 -0500

    Init commit

---
+++ fraud-service-demo/src/main/java/com/hugodesmarques/fraudservicedemo/FraudServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hugodesmarques.fraudservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudServiceDemoApplication.class, args);
+    }
+
+}
